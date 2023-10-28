84f65ea49475b567c9aa83edfcd3851ee6124c18
Chris N <hi.chrisn@gmail.com>
2023-10-28 16:40:53 -0300

    add logo e header

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="sustentabilidade.png" alt="Logo Sustentabilidade"></img>
+      <h1>Coleta Sustentável</h1>
+    </header>
+  );
+}
+
+export default Header;
