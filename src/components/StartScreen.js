54e6087778f503878ea5bb867ad2731af0ee745e
Chris N <hi.chrisn@gmail.com>
2023-10-29 20:55:41 -0300

    updating questions

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,8 +1,8 @@
 export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Bem vindo(a)!</h2>
-      <h3>{numQuestions} questões para testar o seu conhecimento</h3>
+      <h2>Bem vindo(a) ao QUIZ!</h2>
+      <h3>{numQuestions} questões sobre Sustentabilidade</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
